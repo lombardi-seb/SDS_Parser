7cede02bfa43c58347a756521a9a86b8fdb92e04
lombardi-seb <55884828+lombardi-seb@users.noreply.github.com>
2026-06-11 20:07:12 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# --- Oracle Connection ---
+DB_USERNAME = '***********'
+DB_PASSWORD = '***********'
+DB_HOST = '**********.****.it'
+DB_PORT = '1521'
+DB_SERVICE = '**********'
+
+# --- SDS Dictionary ---
+TSV_PATH = 'ghscode_10.txt'
+
+# --- API Biovia Endpoints ---
+LOGIN_URL = "https://************.****.it:9953/foundation/hub/api/v1/security/login"
+LOGOUT_URL = "https://***************.****.it:9953/foundation/hub/api/v1/security/logout"
+DELETE_URL = "https://*************.****.it/cispro/inventory/api/v1/session"
+UPLOAD_URL = "https://***************.****.it/cispro/inventory/api/v1/ghs"
+UPLOAD_ADDITIONAL_URL = "https://****************.****.it/cispro/inventory/api/v1/chemicals"
+
+# --- Tesseract Path ---
+# Customize with your configuration
+TESSERACT_PATH = r"C:\Users\lombardi\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
+# --- OpenRouter Config ---
+OPENROUTER_API_KEY = "sk-or-v1-********************************"
